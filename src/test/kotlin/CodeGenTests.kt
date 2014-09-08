b8da0c20b4af04f5602eeb91836f2233e36a091a
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-09-08 20:47:00 +0400

    Builds.

---
+++ src/test/kotlin/CodeGenTests.kt
@@ -0,0 +1,74 @@
+package org.jetbrains.interpret.tests
+
+import org.junit.*
+import kotlin.test.*
+import java.util.Date
+import org.jetbrains.interpret.*
+
+trait Strings {
+    var name: String
+}
+
+trait Numbers {
+    val count: Int
+    var size: Long
+    val percent: Double
+}
+
+trait Logic {
+    var yesno: Boolean
+}
+
+trait Dates {
+    var created: Date
+}
+
+
+fun Map<String, Any>.toStorage(): Storage {
+    val map = this
+    return object : Storage {
+        override fun get(property: String): Any {
+            return map.get(property)!!
+        }
+        override fun set(property: String, value: Any) {
+            throw UnsupportedOperationException()
+        }
+        override fun get(property: String, propertyType: Class<out Any?>): Any {
+            throw UnsupportedOperationException()
+        }
+        override fun set(property: String, propertyType: Class<out Any?>, value: Any) {
+            throw UnsupportedOperationException()
+        }
+    }
+}
+
+class CodeGenTests {
+    /* class TestGen {
+         fun test() {
+         }
+     }
+
+     Test fun asm_dump() {
+         dump(javaClass<TestGen>())
+         fail()
+     }*/
+
+    Test fun given_database_and_interface_with_string_on_load_should_create_class() {
+        val entity = mapOf("value" to "name").toStorage()
+        val named = entity.interpretAs(javaClass<Strings>())
+        assertEquals("value", named.name)
+    }
+
+    Test fun given_database_and_interface_with_numbers_on_load_should_create_class() {
+        val entity = mapOf(
+                "count" to 12,
+                "size" to 42L,
+                "percent" to 99.9
+                          ).toStorage()
+
+        val numbers = entity.interpretAs(javaClass<Numbers>())
+        assertEquals(12, numbers.count)
+        assertEquals(42L, numbers.size)
+        assertEquals(99.9, numbers.percent)
+    }
+}
